d6038b0171b7681de6354a8bd66248e8bcab9a0f
Emile Belanger <emile.belanger@gmail.com>
2025-03-17 18:46:35 +0000

    Fix 32bit build

--- neo/framework/miniz/minizconf.h
+++ neo/framework/miniz/minizconf.h
@@ -65,6 +65,10 @@
   #endif
 #endif
 
+#ifdef __ANDROID__
+#define z_off64_t off64_t
+#endif
+
 #if defined(__cplusplus) && __cplusplus >= 201103L
 static_assert( sizeof(z_off64_t) == 8, "z_off64_t should be a 64bit type" );
 #endif
